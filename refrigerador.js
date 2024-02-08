12ebf09b211351c03d40f905defffbe39213b499
Mesias02 <146044728+Mesias02@users.noreply.github.com>
2024-02-08 15:18:49 -0500

    Initial commit

---
+++ refrigerador.js
@@ -0,0 +1,24 @@
+'use strict'
+
+class Refrigerador {
+    constructor(producto, cantidad) {
+      this.producto = producto;
+      this.cantidad = cantidad || 0;
+
+    }
+    moreProdduct(newproducto, nuevacantidad){
+        this.producto = newproducto;
+        this.cantidad += nuevacantidad;
+        alert (`Hay ${this.cantidad} unidades de ${this.producto}`);
+    }
+    lessProduct(quitarproducto, consumo){
+        this.producto = quitarproducto;
+        this.cantidad -= consumo;
+        alert (`Hay ${this.cantidad} unideades de ${this.producto}`);
+    }
+
+}
+
+let refrigerador = new Refrigerador("manzana", 5);
+refrigerador.moreProdduct("manzana", 3)
+refrigerador.lessProduct("manzana", 3)
